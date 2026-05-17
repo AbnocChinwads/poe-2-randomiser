3dff713386c03d7ae7005f573ee92421148ad94e
AbnocChinwads <rnemo@hotmail.co.uk>
2026-05-17 21:38:00 +0100

    fix: correct database host environment variable from DB_URL to DB_HOST

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.APP_PORT;
 
 const db = new pg.Client({
   user: process.env.DB_USER,
-  host: process.env.DB_URL,
+  host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
